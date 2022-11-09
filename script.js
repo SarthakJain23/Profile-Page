7fb52062db845d05f823386669ed9276f04929e4
Sarthak Jain <sarthak1280.be21@chitkara.edu.in>
2022-11-09 13:25:58 +0530

    Preloader added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.querySelector('.heading-image').addEventListener('mouseover',()=>{
+document.querySelector('.heading-image').addEventListener('mouseover', () => {
     $('heading-image').addClass('show');
 })
+
+var loader = document.querySelector('.preloader');
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+    loader.classList.add("disappear");
+}
